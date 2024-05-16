eb2606319bf88a35caa2b7fe47a9b9e59389631d
iizardii <10145702-IIZardII@users.noreply.gitlab.com>
2024-05-16 20:59:06 +0100

    Addded test for correct store being called

--- Melior.InterviewQuestion.UnitTests/Tests/Services/PaymentServiceTests.cs
+++ Melior.InterviewQuestion.UnitTests/Tests/Services/PaymentServiceTests.cs
@@ -54,7 +54,7 @@ namespace Melior.InterviewQuestion.UnitTests.Tests.Services
         [TestCaseSource(nameof(FasterPaymentsPaymentServiceTestData))]
         public void MakePayment_FasterPayments_WithLowBalance_IsNotSuccess(string dataStoreType, PaymentScheme paymentScheme)
         {
-            var testbalance = 1000;
+            var testbalance = 1000m;
             var testaccount = new Account { AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments, Balance = testbalance };
             _container.GetMock<IOptionsSnapshot<PaymentServiceOptions>>()
                 .SetupGet(m => m.Value).Returns(new PaymentServiceOptions(dataStoreType));
@@ -85,7 +85,7 @@ namespace Melior.InterviewQuestion.UnitTests.Tests.Services
         [TestCaseSource(nameof(AllPaymentServiceTestData))]
         public void MakePayment_WithCorrectSettings_IsSuccess(string dataStoreType, PaymentScheme paymentScheme)
         {
-            var testbalance = 1000;
+            var testbalance = 1000m;
             var testaccount = new Account { 
                 AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs | AllowedPaymentSchemes.FasterPayments | AllowedPaymentSchemes.Chaps, 
                 Balance = testbalance * 2,
@@ -102,6 +102,81 @@ namespace Melior.InterviewQuestion.UnitTests.Tests.Services
             Assert.That(result.Success, Is.True);
         }
 
+        [TestCaseSource(nameof(AllPaymentServiceTestData))]
+        public void MakePayment_WithCorrectSettings_UpdatesAccount_WithNewBalance(string dataStoreType, PaymentScheme paymentScheme)
+        {
+            var expectedBalance = 1000m;
+            var testaccount = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs | AllowedPaymentSchemes.FasterPayments | AllowedPaymentSchemes.Chaps,
+                Balance = expectedBalance * 2,
+                Status = AccountStatus.Live
+            };
+            _container.GetMock<IOptionsSnapshot<PaymentServiceOptions>>()
+                .SetupGet(m => m.Value).Returns(new PaymentServiceOptions(dataStoreType));
+            _container.GetMock<IAccountDataStore>().Setup(m => m.GetAccount(It.IsAny<string>()))
+                .Returns(testaccount);
+            var request = new MakePaymentRequest { PaymentScheme = paymentScheme, Amount = expectedBalance };
+
+            var result = _sut.MakePayment(request);
+
+            _container.GetMock<IAccountDataStore>().Verify(m => m.UpdateAccount(It.Is<Account>(a => a.Balance == expectedBalance)));
+        }
+
+        [TestCaseSource(nameof(BackupPaymentServiceTestData))]
+        public void MakePayment_WithCorrectSettings_UpdatesAccount_OnBackupDataStore(string dataStoreType, PaymentScheme paymentScheme)
+        {
+            var liveMock = new Mock<IAccountDataStore>();
+            var backupMock = new Mock<IAccountDataStore>();
+            var testbalance = 1000m;
+            var testaccount = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs | AllowedPaymentSchemes.FasterPayments | AllowedPaymentSchemes.Chaps,
+                Balance = testbalance * 2,
+                Status = AccountStatus.Live
+            };
+            _container.GetMock<IOptionsSnapshot<PaymentServiceOptions>>()
+                .SetupGet(m => m.Value).Returns(new PaymentServiceOptions(dataStoreType));
+            backupMock.Setup(m => m.GetAccount(It.IsAny<string>()))
+                .Returns(testaccount);
+            var request = new MakePaymentRequest { PaymentScheme = paymentScheme, Amount = testbalance };
+            _sut = new PaymentService(_container.GetMock<IOptionsSnapshot<PaymentServiceOptions>>().Object, liveMock.Object, backupMock.Object);
+
+            var result = _sut.MakePayment(request);
+
+            backupMock.Verify(m => m.GetAccount(It.IsAny<string>()), Times.Once);
+            backupMock.Verify(m => m.UpdateAccount(It.IsAny<Account>()), Times.Once);
+            liveMock.Verify(m => m.GetAccount(It.IsAny<string>()), Times.Never);
+            liveMock.Verify(m => m.UpdateAccount(It.IsAny<Account>()), Times.Never);
+        }
+
+        [TestCaseSource(nameof(LivePaymentServiceTestData))]
+        public void MakePayment_WithCorrectSettings_UpdatesAccount_OnLiveDataStore(string dataStoreType, PaymentScheme paymentScheme)
+        {
+            var liveMock = new Mock<IAccountDataStore>();
+            var backupMock = new Mock<IAccountDataStore>();
+            var testbalance = 1000m;
+            var testaccount = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs | AllowedPaymentSchemes.FasterPayments | AllowedPaymentSchemes.Chaps,
+                Balance = testbalance * 2,
+                Status = AccountStatus.Live
+            };
+            _container.GetMock<IOptionsSnapshot<PaymentServiceOptions>>()
+                .SetupGet(m => m.Value).Returns(new PaymentServiceOptions(dataStoreType));
+            liveMock.Setup(m => m.GetAccount(It.IsAny<string>()))
+                .Returns(testaccount);
+            var request = new MakePaymentRequest { PaymentScheme = paymentScheme, Amount = testbalance };
+            _sut = new PaymentService(_container.GetMock<IOptionsSnapshot<PaymentServiceOptions>>().Object, liveMock.Object, backupMock.Object);
+
+            var result = _sut.MakePayment(request);
+
+            backupMock.Verify(m => m.GetAccount(It.IsAny<string>()), Times.Never);
+            backupMock.Verify(m => m.UpdateAccount(It.IsAny<Account>()), Times.Never);
+            liveMock.Verify(m => m.GetAccount(It.IsAny<string>()), Times.Once);
+            liveMock.Verify(m => m.UpdateAccount(It.IsAny<Account>()), Times.Once);
+        }
+
         public static IEnumerable<TestCaseData> AllPaymentServiceTestData()
         {
             yield return new TestCaseData(BackupDataStoreName, PaymentScheme.Bacs);
@@ -110,12 +185,6 @@ namespace Melior.InterviewQuestion.UnitTests.Tests.Services
             yield return new TestCaseData(LiveDataStoreName, PaymentScheme.Bacs);
             yield return new TestCaseData(LiveDataStoreName, PaymentScheme.FasterPayments);
             yield return new TestCaseData(LiveDataStoreName, PaymentScheme.Chaps);
-        }
-
-        public static IEnumerable<TestCaseData> BacsPaymentServiceTestData()
-        {
-            yield return new TestCaseData(BackupDataStoreName, PaymentScheme.Bacs);
-            yield return new TestCaseData(LiveDataStoreName, PaymentScheme.Bacs);
         }
 
         public static IEnumerable<TestCaseData> FasterPaymentsPaymentServiceTestData()
@@ -127,6 +196,20 @@ namespace Melior.InterviewQuestion.UnitTests.Tests.Services
         public static IEnumerable<TestCaseData> ChapsPaymentServiceTestData()
         {
             yield return new TestCaseData(BackupDataStoreName, PaymentScheme.Chaps);
+            yield return new TestCaseData(LiveDataStoreName, PaymentScheme.Chaps);
+        }
+
+        public static IEnumerable<TestCaseData> BackupPaymentServiceTestData()
+        {
+            yield return new TestCaseData(BackupDataStoreName, PaymentScheme.Bacs);
+            yield return new TestCaseData(BackupDataStoreName, PaymentScheme.FasterPayments);
+            yield return new TestCaseData(BackupDataStoreName, PaymentScheme.Chaps);
+        }
+
+        public static IEnumerable<TestCaseData> LivePaymentServiceTestData()
+        {
+            yield return new TestCaseData(LiveDataStoreName, PaymentScheme.Bacs);
+            yield return new TestCaseData(LiveDataStoreName, PaymentScheme.FasterPayments);
             yield return new TestCaseData(LiveDataStoreName, PaymentScheme.Chaps);
         }
     }
